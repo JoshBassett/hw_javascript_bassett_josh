52e9a39069ca34890c107a1636e3c5dd409f2fe6
Josh Bassett <jbassett9@gmail.com>
2015-06-12 13:34:32 -0400

    fixed numba function on main.js

--- js/main.js
+++ js/main.js
@@ -4,16 +4,21 @@ function howdy() {
 
 howdy();
 
-var myVar =
-    function numba(string) {
-        if (string < 7) {
+var numba =
+    function (string) {
+        if (string.length < 7) {
             console.log("What a short little word!");
-        } else if (string > 7) {
+        } else if (string.length > 7) {
             console.log("I'm sorry, but that's too many to count");
-        } else if (string === 7) {
+        } else if (string.length === 7) {
             console.log("7, What a perfect choice!")
         };
     };
+
+    numba("7");
+    numba("seventy");
+    numba("university");
+
 
 function inception(callback, value) {
     callback(value);
