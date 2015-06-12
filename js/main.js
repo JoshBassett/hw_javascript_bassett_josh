ac3910869beae6be0f79cf0005cc48718cb2895d
Josh Bassett <jbassett9@gmail.com>
2015-06-12 11:55:37 -0400

    Started numba js code - not working Initial attempt to code the numba funtion. It is not going well so far. Several issues: var name probably not correct, if/else statements not working.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,23 @@ function howdy() {
 }
 
 howdy();
+
+var myVar =
+    function numba(string) {
+        if (string < 7) {
+            console.log("What a short little word!");
+        } else (string > 7) 
+            console.log("I'm sorry, but that's too many to count");
+         else (string === 7) {
+            console.log("7, What a perfect choice!")
+        };
+    };
+
+
+
+//function inception(callback, value) {
+//	callback(value);
+//};
+
+//var callback = 2
+//var value = 5
